f88269c54e57f1873d7f9a931e69463d37336bd6
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-01-26 21:31:58 -0300

    add inicio do instact

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,22 @@
+
+<nav class="navbar navbar-light fixed-top bg-white border-bottom">
+    <div class="container-fluid" style="width: 900px; max-width: 100%">
+        <div class="navbar-header">
+            <img src="{{asset('images/logo.png')}}" width="100">
+        </div>
+
+        <div class="navbar-nav flex-row">
+            <a href="posts/create" class="nav-link me-3">
+                <i class="bi bi-plus-square fs-3"></i>
+            </a>
+
+            <a href="posts/notifications" class="nav-link me-3">
+                <i class="bi bi-heart fs-3"></i>
+            </a>
+
+            <a href="users/config" class="nav-link me-3">
+                <i class="bi bi-person-circle fs-3"></i>
+            </a>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/components/posts-card.blade.php
@@ -0,0 +1,17 @@
+<div class="card mb-3" style="width: 600px; max-width: 100%">
+    <div class="card-header bg-white border-0">
+        <i class="bi bi-person-circle fs-5 me-2"></i>
+        <span class="fw-bold">João Silva</span>
+    </div>
+
+    <img src="https://i.imgur.com/BEqFxWf.png" class="card-img rounded-0">
+    <div class="card-body">
+        <p class="card-text">
+            <span class="fw-bolt">João Silva</span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            ratione, odit necessitatibus sint minus rem ex, eius quibusdam minima adipisci
+             quam architecto impedit ab! Tempora fuga totam qui ea quisquam!
+
+        </p>
+    </div>
+</div>
