033973cb388550360b920ede854a473d6abebf40
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-02-01 22:54:41 -0300

    add refactor

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Services\PostService;
 use App\Http\Requests\StorePostRequest;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\UpdatePostRequest;
 
 class PostController extends Controller
 {
+    /**
+     * @var PostService
+     */
+    protected $service;
+
+    public function __construct(PostService $service)
+    {
+        $this->service = $service;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -40,17 +50,15 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        $user = auth()->user();
-        $path = $request->photo->store('public/images');
-        $url = Storage::url($path);
+        $input = $request->only('description');
+        $input['user_id'] = auth()->id();
 
-        $post = Post::create([
-            'image' => $url,
-            'description' => $request->description,
-            'user_id' =>$user->id
-        ]);
+        $response = $this->service->store($input, $request->photo);
 
-        return redirect('/dashboard')->with('success', 'Post criado com sucesso');
+        if (!$response['success']) {
+            return back()->with('error', $response['message']);
+        }
+        return redirect('dashboard');
     }
     /**
      * Display the specified resource.
